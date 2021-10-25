f1248cbd2a9410176cfeecc4e98419f30e2d9f73
Diego Andrés Barbosa Trujillo <da.barbosa10@uniandes.edu.co>
2021-10-24 19:24:30 -0500

    Add files via upload

---
+++ Playing/Small_Challenges/Easiest/nearest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 24 19:05:09 2021
+
+@author: diegobarbosa
+"""
+def nearest_value(values: set, b: int) -> int:
+    a=sorted(list(values))
+    c=[]
+    for i in range(len(a)):
+        c.append(abs(a[i]-b))
+    mn=min(c)
+    return a[c.index(mn)]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
+    assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
+    assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
+    assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
+    assert nearest_value({-1, 2, 3}, 0) == -1
+    print("Coding complete? Cool!")
+
